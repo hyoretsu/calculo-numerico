725f0fcc5c54999a12c44ab3686ea60bc0a0fec1
Aran Leite <hyoretsu@gmail.com>
2022-09-07 11:18:13 -0300

    Método da falsa posição (zeros da função)

--- numerical-methods/src/index.ts
+++ numerical-methods/src/index.ts
@@ -37,3 +37,37 @@ export const bisection = ({ func, interval: [a, b], precision }: Bisection) => {
 
     return { iterations, interval: [a, b] };
 };
+
+interface FalsePosition {
+    func: (x: number) => number;
+    interval: [number, number];
+    precision: number;
+}
+
+export const falsePosition = ({ func, interval: [a, b], precision }: FalsePosition) => {
+    let condition1, condition2;
+    let iterations = 0;
+
+    while (true) {
+        const results = [func(a), func(b)];
+
+        condition1 = Math.abs(b - a) < precision;
+        condition2 = Math.abs(results[0]) < precision;
+        if (condition1 || condition2) break;
+
+        const newPoint = (a * func(b) - b * func(a)) / (func(b) - func(a));
+        const newResult = func(newPoint);
+        switch (Math.sign(newResult)) {
+            case Math.sign(results[0]):
+                a = newPoint;
+                break;
+            case Math.sign(results[1]):
+                b = newPoint;
+                break;
+        }
+
+        iterations += 1;
+    }
+
+    return { iterations, interval: [a, b] };
+};
