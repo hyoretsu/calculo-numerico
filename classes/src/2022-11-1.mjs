2543f34940117660517cf0d570cde2d8d976275b
Aran Leite <hyoretsu@gmail.com>
2022-11-09 12:15:05 -0300

    Bump

--- classes/src/2022-11-1.mjs
+++ classes/src/2022-11-1.mjs
@@ -5,22 +5,22 @@ let error, func, result, x;
 func = '(1 + x^2)^(-1/2)';
 x = [0, 4];
 
-({ result, error } = trapezoidalRule({ pointN: 2, func, x }));
+({ result } = trapezoidalRule({ pointN: 2, func, x }));
 console.log(`2 pontos: ${result}`);
-({ result, error } = trapezoidalRule({ pointN: 3, func, x }));
+({ result } = trapezoidalRule({ pointN: 3, func, x }));
 console.log(`3 pontos: ${result}`);
 ({ result, error } = trapezoidalRule({ pointN: 9, func, x }));
-console.log(`9 pontos: ${result} ${error}`);
+console.log(`9 pontos: ${result} erro ${error}`);
 
 console.log(
     '-------------------------------------------------------------------------------------------------------------------------------------------------',
 );
 
-const pointN = 10;
+const pointN = 11;
 func = 'e^x';
 x = [0, 1];
 
-({ result } = trapezoidalRule({ pointN, func, x }));
-console.log(`Regra do trapézio repetida, 10 pontos: ${result}`);
-({ result } = simpsonRule13({ pointN, func, x }));
-console.log(`Regra 1/3 de Simpson repetida, 10 pontos: ${result}`);
+({ result, error } = trapezoidalRule({ pointN, func, x }));
+console.log(`Regra do trapézio repetida, 10 intervalos: ${result} erro ${error}`);
+({ result, error } = simpsonRule13({ pointN, func, x }));
+console.log(`Regra 1/3 de Simpson repetida, 10 intervalos: ${result} erro ${error}`);
